977e6bd983929fac21bf0a135aa85b7aac8097d7
mkanazu <mknz@users.noreply.github.com>
2020-11-09 19:25:51 +0100

    Improve cleanups

--- ipython_launcher_template.sh
+++ ipython_launcher_template.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-
 # Save current window id
 CURRENT_WINDOW=$(wmctrl -l | head -1 | cut -d ' ' -f 1)
 
 # Run server background
 PLOTLY_FIG_DIR=$(pwd)/plotly_figs python3 $VIEWER_DIR/server.py &> /dev/null &
+SERVER_PID=$!
 
 # Run plot viewer background
 chromium-browser --app=file://$VIEWER_DIR/client.html --window-size="800,600" &> /dev/null &
 VIEWER_PID=$!
 
-# Restore windowfocus
-sleep 0.7
+# Wait 1 sec and restore window focus from the browser
+sleep 1
 wmctrl -i -a $CURRENT_WINDOW
 
-ipython -i
+ipython -i $1
 
 # Cleanup
-SERVER_PIDS=$(ps aux|grep python|grep server.py|awk '{print $2}')
-kill -KILL $SERVER_PIDS &> /dev/null
+kill -KILL $SERVER_PID
+wait $SERVER_PID &> /dev/null
 kill -KILL $VIEWER_PID &> /dev/null
+rmdir ./plotly_figs &> /dev/null
