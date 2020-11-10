e32d8b5fa332bb17fb757c65147d748bb5ba0346
mkanazu <mknz@users.noreply.github.com>
2020-11-10 19:57:12 +0100

    lint

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
-var ws = new WebSocket('ws://127.0.0.1:8765/');
-var figContainer = document.getElementById('fig-container');
-var fileIds = new Set();
-var initDone = false;
+const ws = new WebSocket('ws://127.0.0.1:8765/');
+const figContainer = document.getElementById('fig-container');
+const fileIds = new Set();
+let initDone = false;
 
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
@@ -16,11 +16,10 @@ const SendFileIds = async () => {
 // Keep sending list of files forever
 ws.onopen = SendFileIds;
 
-
 function showFig (fileId) {
   for (let i = 0; i < figContainer.children.length; i++) {
-    let figSubContainer = figContainer.children[i];
-    if ((figSubContainer.id == fileId + '_sc')) {
+    const figSubContainer = figContainer.children[i];
+    if ((figSubContainer.id === fileId + '_sc')) {
       figSubContainer.style.display = 'block';
     } else {
       figSubContainer.style.display = 'none';
@@ -28,10 +27,10 @@ function showFig (fileId) {
   }
 }
 
-const init = async () => {
+const init = async function () {
   // Wait 2 seconds for loading, then show last added figure
   // Show loading message
-  let loading = document.createElement('p');
+  const loading = document.createElement('p');
   loading.id = 'loading';
   loading.textContent = 'Loading...';
   document.body.appendChild(loading);
@@ -40,7 +39,7 @@ const init = async () => {
   await sleep(2000);
 
   // Show last figure
-  let fileIdsArr = Array.from(fileIds);
+  const fileIdsArr = Array.from(fileIds);
   fileIdsArr.sort();
   showFig(fileIdsArr[fileIdsArr.length - 1]);
 
@@ -48,25 +47,25 @@ const init = async () => {
   loading.remove();
 
   // Show link list
-  var figList = document.getElementById('fig-list');
+  const figList = document.getElementById('fig-list');
   figList.style.display = 'block';
   initDone = true;
 };
 init();
 
-function updateLinkList() {
-  var figList = document.getElementById('fig-list');
+function updateLinkList () {
+  const figList = document.getElementById('fig-list');
   if (!initDone) { figList.style.display = 'none'; }
   // Init figure link list
   while (figList.firstChild) {
     figList.removeChild(figList.firstChild);
   }
   // Update figure link list
-  let fileIdsArr = Array.from(fileIds);
+  const fileIdsArr = Array.from(fileIds);
   fileIdsArr.sort();
   for (let i = 0; i < fileIdsArr.length; i++) {
-    let fileId = fileIdsArr[i];
-    let link = document.createElement('button');
+    const fileId = fileIdsArr[i];
+    const link = document.createElement('button');
     link.textContent = fileId;
     link.onclick = function () { showFig(fileId); };
     figList.appendChild(link);
@@ -74,26 +73,26 @@ function updateLinkList() {
 }
 
 ws.onmessage = function (event) {
-  var data = JSON.parse(event.data);
-  if (!fileIds.has(data['id'])) { // New file
-    let fileId = data['id'];
+  const data = JSON.parse(event.data);
+  if (!fileIds.has(data.id)) { // New file
+    const fileId = data.id;
     fileIds.add(fileId);
     console.log('Added ', fileId);
-    var fig = data['fig'];
+    const fig = data.fig;
 
     // Create figure element
-    var newFig = document.createElement('div');
+    const newFig = document.createElement('div');
     newFig.id = fileId;
     Plotly.newPlot(newFig, fig.data, fig.layout);
 
     // Create figure sub container
-    var figSubContainer = document.createElement('div');
+    const figSubContainer = document.createElement('div');
     figSubContainer.id = newFig.id + '_sc';
 
     // Set title
-    var title = document.createElement('h4');
+    const title = document.createElement('h4');
     // Remove ext
-    var titleText = fileId.split('.').slice(0, -1).join('.');
+    const titleText = fileId.split('.').slice(0, -1).join('.');
     title.textContent = titleText;
 
     figSubContainer.appendChild(title);
